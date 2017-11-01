21a036f0069f232831f7f2ba3c82b3efdd4df1dd
Alexsaphir <alex.game@sfr.fr>
2017-11-01 14:45:44 +0100

    Remove anim, add method 2

--- TP5.py
+++ TP5.py
@@ -20,20 +20,22 @@ def solex(X,ct):
 
 print('Choix de la vitesse de transport c')
 #c = float(input('c = '))
-c = 2
+c = -2
 
-Ns = 10
+Ns = 1000
 h = 1./(Ns + 1.)
 
 X = linspace(0.,1.,Ns+1)
 Xh = X[0:Ns]
 
-dt = .001
+dt = .0001
 T=1.
 M = int((T/dt) - 1)
 
-meth = 1
-
+meth = 2
+#Uj temps actuel
+#Ujm temps precedent
+#Tjn temps suivant
 Uj = U0(Xh)
 Ujm = zeros(shape(U0))
 Ujn = zeros(shape(U0))
@@ -46,29 +48,60 @@ Uj, Ujm = Ujm, Uj
 Ujn, Uj = Uj, Ujn
 
 A= diag(-ones(Ns-1),1)-diag(ones(Ns-1),-1)+2.*eye(Ns)
-
+A=A/h/h
 #Erreur
 Err = 0
 Errn = 0
 
-A=A/h/h
-line1, = plot(linspace(0,1,100), solex(linspace(0,1,100),T), label = 'sol exacte')
+#line1, = plot(linspace(0,1,100), solex(linspace(0,1,100),T), label = 'sol exacte')
 
 for j in arange(1, M):
-    for i in arange(1,Ns):
-        if( meth == 1):
-            Ujn = 2.*Uj-Ujm-c*c*dt*dt*(A*Uj)
-        #if( meth ==2 ):
-
+    if( meth == 1):
+        for i in arange(1,Ns):
+                Ujn = 2.*Uj-Ujm-c*c*dt*dt*(A.dot(Uj))
+    if( meth == 2 ):
+        Ujn = solve( (eye(Ns)+c*c*dt*dt*A), 2.*Uj-Ujm)
             
-        #Calcul de l'erreur
-        U=solex(Xh,j*dt*c)
-        Errn = amax(absolute(U - Ujn))
-        if (Err < Errn):
-            Err = Errn
+    #Calcul de l'erreur
+    U=solex(Xh,j*dt*c)
+    Errn = amax(absolute(U - Ujn))
+    if (Err < Errn):
+        Err = Errn
 
-        Uj, Ujm = Ujm, Uj
-        Ujn, Uj = Uj, Ujn
-plot(Xh, Uj)
+    Uj, Ujm = Ujm, Uj
+    Ujn, Uj = Uj, Ujn
 
+plot(Xh, Uj,label="Approché")
+plot(linspace(0,1,500),solex(linspace(0,1,500),T*c),label='exacte')
+
+xlabel('X')
+ylabel('Y')
+legend()
 show()
+disp(Err)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
